a6d84c52670b0ff1d86f0c8eb6efed3877300ae6
Dominik Süß <dominik@suess.wtf>
2025-09-02 13:50:52 +0200

    feat: python instrumentation

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ PODMAN = $(shell if command -v podman &>/dev/null; then echo podman; else echo d
 all: clients dashboards
 
 .PHONY: clients
-clients: client-go
+clients: client-go client-python
 
 .PHONY: client-go
 client-go:
 	$(PODMAN) run -it --rm -v $(PWD):/work:Z -u 0 --workdir /work otel/weaver registry generate -r ./semconv/ go ./generated/client/go 
+.PHONY: client-python
+client-go:
+	$(PODMAN) run -it --rm -v $(PWD):/work:Z -u 0 --workdir /work otel/weaver registry generate -r ./semconv/ python ./generated/client/python 
 .PHONY: dashboards
 dashboards:
 	$(PODMAN) run -it --rm -v $(PWD):/work:Z -u 0 --workdir /work otel/weaver registry generate -r ./semconv/ dashboards ./generated/dashboards
--- applications/frontend/app.py
+++ applications/frontend/app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, flash, g, request, Response
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
+from o11y_lib import http
 import requests
+import time
 
 app = Flask(__name__)
 app.secret_key = b'a-very-good-secret'
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+durations = http.ServerRequestDuration()
+
+@app.before_request
+def start_timer():
+    g.method = request.method
+    g.scheme = request.scheme
+    g.route = request.path
+    g.start = time.time()
+
+@app.after_request
+def log_details(response: Response):
+    durations.with_attr(g.method,g.scheme,response_status_code=response.status,route=g.route).observe(time.time() - g.start)
+    return response
 
 def get_books():
     response = requests.get("http://localhost:8080/books")
